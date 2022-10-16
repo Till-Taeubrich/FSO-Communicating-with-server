399f0c88767cc705893c4d89742c46d7d8a3eec6
till taeubrich <till.taeubrich@gmail.com>
2022-10-16 17:44:54 +1100

    finish exercise 2.4 & 2.5

--- src/Sum.js
+++ src/Sum.js
@@ -3,5 +3,5 @@ import React from 'react';
 export default function Sum({ course }) {
   const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
-  return <div>{sum}</div>;
+  return <div>total of {sum} exercises</div>;
 }
