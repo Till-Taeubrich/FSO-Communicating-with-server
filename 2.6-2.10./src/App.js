ece9caefc79ae39d92fca3d0f44802e3d6375030
till taeubrich <till.taeubrich@gmail.com>
2022-10-18 10:28:32 +1100

    finish exercis e 2.9

--- 2.6-2.10./src/App.js
+++ 2.6-2.10./src/App.js
@@ -4,12 +4,23 @@ import uniqid from 'uniqid'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: uniqid(), number: '040-123456' }
+    { name: 'Arto Hellas', id: uniqid(), number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]) 
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   
+  const handleFiltering = (e) => {
+    const filteredPersons = persons.filter((person) => {
+      return person.name.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase());
+    });
+
+    setPersons(filteredPersons);
+  };
+
   const handleNameChange = (e) => {
     setNewName(e.target.value)
   }
@@ -38,6 +49,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={handleFiltering} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input onChange={handleNameChange} />
@@ -46,15 +61,19 @@ const App = () => {
           number: <input onChange={handleNumberChange} />
         </div>
         <div>
-          <button onClick={addPerson} type="submit">add</button>
+          <button onClick={addPerson} type="submit">
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <div key={person.id}>{person.name} {person.number}</div>
-        )}
+      {persons.map((person) => (
+        <div key={person.id}>
+          {person.name} {person.number}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default App
