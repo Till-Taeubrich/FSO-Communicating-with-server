d19d37d52e9373b5bc41d619c7d7190366e850a2
till taeubrich <till.taeubrich@gmail.com>
2022-10-17 14:03:29 +1100

    finish exercise 2.6

--- 2.6-2.10./src/App.js
+++ 2.6-2.10./src/App.js
@@ -1,24 +1,43 @@
 import { useState } from 'react'
+import uniqid from 'uniqid'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', id: uniqid() }
   ]) 
+
   const [newName, setNewName] = useState('')
+  
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+
+    const newPerson = {
+      name: newName,
+      id: uniqid(),
+    }
+
+    setPersons(persons.concat(newPerson))
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addPerson} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+        <div key={person.id}>{person.name}</div>
+        )}
     </div>
   )
 }
